1767f954fbd7522b5a5ebff15eef1a6e608fd11f
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2026-03-02 14:09:30 +0300

    Added Order and OrderItem models

--- ecommerce_api/products/admin.py
+++ ecommerce_api/products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Product, Category
+from .models import Order, OrderItem
 
 
 @admin.register(Category)
@@ -12,3 +13,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "price", "stock_quantity", "category")
     list_filter = ("category",)
     search_fields = ("name",)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at", "is_completed")
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "product", "quantity")
--- ecommerce_api/products/models.py
+++ ecommerce_api/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -20,3 +21,29 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.username}"
+
+    @property
+    def total_price(self):
+        return sum(item.total_price for item in self.items.all())
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
