b94165d0ee2f9d688f2b096f0c829417957dd8e8
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2026-03-02 13:40:27 +0300

    Added filtering, search, and ordering to Product API

--- ecommerce_api/products/views.py
+++ ecommerce_api/products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['category', 'price', 'stock_quantity']
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'created_at']
